09441940bbab94e952eab38d7d5d3658f2495ad6
Jahaziel4587 <jahaziel4587@gmail.com>
2025-08-05 17:19:52 -0600

    Agrega .gitignore y limpia archivos ignorados

---
+++ app/collection/impresion/page.tsx
@@ -0,0 +1,7 @@
+export default function ImpresionPage() {
+  return (
+    <div className="min-h-screen flex items-center justify-center">
+      <h1 className="text-2xl font-bold">Colección va aquí</h1>
+    </div>
+  );
+}
