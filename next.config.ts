5c806fc78aba43d04d2e92e9d40a786404141892
Jahaziel4587 <jahaziel4587@gmail.com>
2025-08-05 21:35:04 -0600

    activar appDir

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   experimental: {
-    serverActions: true,
+    appDir: true,
   },
-  // Asegúrate de que esto está habilitado (aunque ya no es obligatorio, a veces ayuda)
-  appDir: true,
 };
 
 export default nextConfig;
