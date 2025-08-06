1328a8f346e4c4eea6ebbb3041a85f7908d57f16
Jahaziel4587 <jahaziel4587@gmail.com>
2025-08-05 21:11:26 -0600

    intento mil8mil

--- next.config.ts
+++ next.config.ts
@@ -1,5 +1,10 @@
-import type { NextConfig } from "next";
-
-const nextConfig= {};
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  experimental: {
+    serverActions: true,
+  },
+  // Asegúrate de que esto está habilitado (aunque ya no es obligatorio, a veces ayuda)
+  appDir: true,
+};
 
 export default nextConfig;
