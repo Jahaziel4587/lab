013de10e8b6103b694c59fd20293d9ca7b62c381
Jahaziel Garza <victor.garzamnz@uanl.edu.mx>
2026-08-31 21:06:26 -0600

    calendario - history

--- app/components/Clientlayout.tsx
+++ app/components/Clientlayout.tsx
@@ -223,7 +223,7 @@ export default function ClientLayout({
               href="/calendario"
               className="hover:text-white transition"
             >
-              Order Overview
+              History
             </Link>
             <Link
               data-tutorial="nav-about"
@@ -408,7 +408,7 @@ export default function ClientLayout({
                 onClick={cerrarMenu}
                 className={mobileLinkClass}
               >
-                Order Overview
+                History
               </Link>
 
               <Link
